3234039a13f2288acdee23bd6a1c4a01a61b6b83
Muszchrom <altair2468@o2.pl>
2024-09-29 01:03:04 +0200

    Fix: app fails when data ind db exists

--- Backend/app/src/main/java/com/example/demo/LoadDatabase.java
+++ Backend/app/src/main/java/com/example/demo/LoadDatabase.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import com.example.demo.entities.Category;
 import com.example.demo.entities.Photo;
@@ -22,26 +23,30 @@ public class LoadDatabase {
   @Bean
   CommandLineRunner initDatabase(CategoryRepository categoryRepository, PlaceRepository placeRepository, PhotoRepository photoRepository) {
     return args -> {
-      Category category1 = new Category("Grecja", "grecja");
-      Category category2 = new Category("Góry stołowe", "gory-stolowe");
-      log.info("Preloading " + categoryRepository.save(category1));
-      log.info("Preloading " + categoryRepository.save(category2));
-
-      Place place1 = new Place(category1, "Santorini", "santorini");
-      Place place2 = new Place(category1, "Ateny", "ateny");
-      Place place3 = new Place(category2, "Mglisty szlak", "mglisty-szlak");
-      log.info("Preloading " + placeRepository.save(place1));
-      log.info("Preloading " + placeRepository.save(place2));
-      log.info("Preloading " + placeRepository.save(place3));
-      
-      Photo photo1 = new Photo("hashedfile1", "jpg", place1);
-      Photo photo2 = new Photo("hashedfile2", "jpg", place2);
-      Photo photo3 = new Photo("hashedfile3", "jpg", place3);
-      Photo photo4 = new Photo("hashedfile4", "jpg", place3);
-      log.info("Preloading " + photoRepository.save(photo1));
-      log.info("Preloading " + photoRepository.save(photo2));
-      log.info("Preloading " + photoRepository.save(photo3));
-      log.info("Preloading " + photoRepository.save(photo4));
+      try {
+        Category category1 = new Category("Grecja", "grecja");
+        Category category2 = new Category("Góry stołowe", "gory-stolowe");
+        log.info("Preloading " + categoryRepository.save(category1));
+        log.info("Preloading " + categoryRepository.save(category2));
+  
+        Place place1 = new Place(category1, "Santorini", "santorini");
+        Place place2 = new Place(category1, "Ateny", "ateny");
+        Place place3 = new Place(category2, "Mglisty szlak", "mglisty-szlak");
+        log.info("Preloading " + placeRepository.save(place1));
+        log.info("Preloading " + placeRepository.save(place2));
+        log.info("Preloading " + placeRepository.save(place3));
+        
+        Photo photo1 = new Photo("hashedfile1", "jpg", place1);
+        Photo photo2 = new Photo("hashedfile2", "jpg", place2);
+        Photo photo3 = new Photo("hashedfile3", "jpg", place3);
+        Photo photo4 = new Photo("hashedfile4", "jpg", place3);
+        log.info("Preloading " + photoRepository.save(photo1));
+        log.info("Preloading " + photoRepository.save(photo2));
+        log.info("Preloading " + photoRepository.save(photo3));
+        log.info("Preloading " + photoRepository.save(photo4));
+      } catch (DataIntegrityViolationException ex) { // lazy ahh solution
+        log.info("Data already preloaded");
+      }
     };
   }
 }
