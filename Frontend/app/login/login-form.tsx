e2aaeeaae08de6682010d98f0db99d5be6e81ec8
Muszchrom <altair2468@o2.pl>
2024-10-11 22:12:55 +0200

    Added login route

---
+++ Frontend/app/login/login-form.tsx
@@ -0,0 +1,99 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  username: z.string().min(3, {
+    message: "Username must be at least 3 characters."
+  }).max(24, {
+    message: "Username must be shoter than 25 characters."
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 6 characters."
+  }).max(255, {
+    message: "Password must be shoter than 256 characters."
+  })
+})
+
+export function LoginForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+      password: ""
+    }
+  })
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    const res = await fetch("http://localhost:8081/auth/login", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(values)
+    })
+    const text = await res.text();
+    console.log(text)
+    res.status == 200 ? console.log("great") : console.log("not great")
+    console.log(values)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+          />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your password.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+          />
+        <div className="flex justify-end">
+          <Button type="submit">Submit</Button>
+        </div>
+      </form>
+    </Form>
+  )
+}
---
+++ Frontend/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Card } from "@/components/ui/card";
+import { LoginForm } from "./login-form";
+
+export default function LogIn() {
+  return (
+    <main className="flex-[1_1_0]">
+      <div className="border-b container flex justify-center items-center h-full">
+        <div className="max-w-xs w-full">
+          <LoginForm />
+        </div>
+      </div>
+    </main>
+  );
+}
